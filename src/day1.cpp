b0af6ff0978152d8890742a1f35c2a08c378099f
0x24d <daniel.longfield@tuta.io>
2018-12-02 17:50:24 +0000

    Add day 1 part 2 answer, part 2 is slow

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,18 +1,38 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <iterator>
 
 int main() {
     std::ifstream inputFile;
-    std::string readLine;
-    int totalResult = 0;
+    std::vector<int> fileIntegers;
     inputFile.open("../inputs/day1.txt");
-    if (inputFile.is_open()) {
-        while (getline(inputFile, readLine)) {
-            totalResult += std::stoi(readLine);
-        }
-        inputFile.close();
-    }
+    std::istream_iterator<int> input(inputFile);
+    std::copy (input, std::istream_iterator<int>(), std::back_inserter(fileIntegers));
+
+    // part 1
+    int totalResult = 0;
+    totalResult = std::accumulate(fileIntegers.begin(), fileIntegers.end(), totalResult);
     std::cout << "Resulting frequency is: " << totalResult << "\n";
+
+    // part 2
+    bool duplicateNumber = false;
+    int duplicateFrequency = 0;
+    std::vector<int> pastFrequencies;
+    do {
+        for (int i : fileIntegers) {
+            duplicateFrequency += i;
+            if (std::find(pastFrequencies.begin(), pastFrequencies.end(), duplicateFrequency) != pastFrequencies.end()) {
+                duplicateNumber = true;
+                break;
+            } else {
+                pastFrequencies.push_back(duplicateFrequency);
+            }
+        }
+    } while (!duplicateNumber);
+    std::cout << "Got duplicate frequency: " << duplicateFrequency << "\n";
     return 0;
 }
