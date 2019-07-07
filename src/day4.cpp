45de9fbbcf7648a8fe61f63cded973766cff8e37
0x24d <daniel.longfield@tuta.io>
2019-07-07 13:52:42 +0100

    Complete day 4 part 1

--- src/day4.cpp
+++ src/day4.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <string>
 
 int main() {
@@ -23,28 +24,58 @@ int main() {
     return aTimestamp < bTimestamp;
   });
 
-  std::map<int, int> timesAsleep;
+  std::deque<std::pair<int, std::deque<int>>> timesAsleep;
   int guardNumber;
-  int lastTime;
+  int timeAsleep = -1;
   for (std::string line : logLines) {
     std::string lineType = line.substr(19, 1);
     if (lineType == "G") {
       int hashIndex = line.find('#');
       int spaceIndex = line.find(' ', hashIndex);
-      guardNumber = std::atoi(line.substr(hashIndex + 1, spaceIndex - hashIndex - 1).c_str());
-      timesAsleep.insert({guardNumber, 0});
+      guardNumber = std::atoi(
+          line.substr(hashIndex + 1, spaceIndex - hashIndex - 1).c_str());
+      timesAsleep.push_back({guardNumber, std::deque<int>(60, 0)});
     } else if (lineType == "w") {
-      int timeAsleep = std::atoi(line.substr(15, 2).c_str()) - lastTime;
-      timesAsleep[guardNumber] += timeAsleep;
+      auto el = std::find_if(timesAsleep.begin(), timesAsleep.end(),
+                             [&guardNumber](std::pair<int, std::deque<int>> p) {
+                               return p.first == guardNumber;
+                             });
+      if (el != timesAsleep.end()) {
+        int timeAwake = std::atoi(line.substr(15, 2).c_str());
+        for (int i = timeAsleep; timeAsleep != -1 && i < timeAwake; ++i) {
+          el->second[i] += 1;
+        }
+      }
     } else {
-      lastTime = std::atoi(line.substr(15, 2).c_str());
+      timeAsleep = std::atoi(line.substr(15, 2).c_str());
     }
   }
-  auto mostAsleep = std::max_element(timesAsleep.begin(), timesAsleep.end(), [](std::pair<int, int> a, std::pair<int, int> b) {
-    return a.second < b.second;
-  });
-  std::cout << "Most asleep guard: " << (*mostAsleep).first << "\n";
-  std::deque<std::pair<int, int>> startEndTimes;
+  std::map<int, int> guardsAsleep;
+  for (auto pair : timesAsleep) {
+    auto total = std::accumulate(pair.second.begin(), pair.second.end(), 0);
+    guardsAsleep.insert({pair.first, total});
+  }
+  auto mostAsleep =
+      std::max_element(guardsAsleep.begin(), guardsAsleep.end(),
+                       [](std::pair<int, int> a, std::pair<int, int> b) {
+                         return a.second < b.second;
+                       });
+
+  auto guardRecord = std::find_if(
+      timesAsleep.begin(), timesAsleep.end(),
+      [&mostAsleep](auto p) { return p.first == mostAsleep->first; });
+
+  if (guardRecord != timesAsleep.end()) {
+    auto maxEl = std::max_element(guardRecord->second.begin(),
+                                  guardRecord->second.end());
+    for (int i = 0; i < guardRecord->second.size(); ++i) {
+      if (guardRecord->second[i] == *maxEl) {
+        std::cout << (mostAsleep->first) << " * " << i << " = "
+                  << (mostAsleep->first * i) << "\n";
+        break;
+      }
+    }
+  }
 
   return 0;
 }
