5f101329ce8ed5d593c1f7b644dde600b241d942
0x24d <daniel.longfield@tuta.io>
2019-07-07 20:58:40 +0100

    Add day 5 part 2

--- src/day5.cpp
+++ src/day5.cpp
@@ -1,6 +1,22 @@
+#include <algorithm>
 #include <deque>
 #include <fstream>
 #include <iostream>
+
+void processChar(std::deque<char> &processedUnits, const int unit) {
+  if (processedUnits.empty()) {
+    processedUnits.emplace_back(unit);
+  } else {
+    auto lastUnit = processedUnits.back();
+    if ((tolower(lastUnit) == tolower(unit)) &&
+        ((std::isupper(lastUnit) && std::islower(unit)) ||
+         (std::islower(lastUnit) && std::isupper(unit)))) {
+      processedUnits.pop_back();
+    } else {
+      processedUnits.push_back(unit);
+    }
+  }
+}
 
 int main() {
   std::deque<char> processedUnits;
@@ -9,23 +25,34 @@ int main() {
   if (inputFile.is_open()) {
     char unit;
     while (inputFile.get(unit)) {
-      if (processedUnits.empty()) {
-        processedUnits.emplace_back(unit);
-      } else {
-        auto lastUnit = processedUnits.back();
-        if ((tolower(lastUnit) == tolower(unit)) &&
-            ((std::isupper(lastUnit) && std::islower(unit)) ||
-             (std::islower(lastUnit) && std::isupper(unit)))) {
-          processedUnits.pop_back();
-        } else {
-          processedUnits.push_back(unit);
-        }
-      }
+      processChar(processedUnits, unit);
     }
     inputFile.close();
   }
 
   std::cout << "Number of units remaining: " << processedUnits.size() << "\n";
+
+  // part 2
+  std::deque<int> numberOfUnits(26, 0);
+  for (int i = 0; i < numberOfUnits.size(); ++i) {
+    processedUnits.clear();
+    inputFile.open("../inputs/day5.txt");
+    if (inputFile.is_open()) {
+      char unit;
+      while (inputFile.get(unit)) {
+        auto charToEmit = static_cast<char>(i + 97);
+        if (std::tolower(unit) != charToEmit) {
+          processChar(processedUnits, unit);
+        }
+      }
+    }
+    inputFile.close();
+    numberOfUnits[i] = processedUnits.size();
+  }
+
+  auto smallestPolymer =
+      std::min_element(numberOfUnits.begin(), numberOfUnits.end());
+  std::cout << "Smallest polymer: " << *smallestPolymer << "\n";
 
   return 0;
 }
